209cfacb1f5332e1bf2e7a13483856a0c164df1b
Nhu Dinh <hnd@umbraco.dk>
2025-08-22 17:13:51 +0700

    Added project

---
+++ Program.cs
@@ -0,0 +1,31 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+builder.Services.AddHostedService<LanguageSeeder>();
+builder.Services.AddHostedService<DictionarySeeder>();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
